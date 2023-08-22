7c1b615dea0fe24114685eddead67b60a6b5ca6f
Photon9 <971991@gmail.com>
2023-08-22 12:34:02 +1000

    First commit

---
+++ website/auth.py
@@ -0,0 +1,61 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email=email).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user, remember=True)
+            flash('Logged in successfully!', category='success')
+            return redirect(url_for('views.home'))
+        else:
+            flash('Login failed')
+
+    return render_template("login.html", user=current_user)
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method =='POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        email_valid = True
+        acceptable_password = True
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already in use', category='error')
+        if not email_valid:
+            flash('Email not valid', category='error')
+        elif len(firstName) < 1:
+            flash('Gotta have a first name', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match', category='error')
+        elif not acceptable_password:
+            flash('You need a better password', category='error')
+        else:
+            new_user = User(email=email, firstName=firstName, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account created!', category='success')
+            login_user(user, remember=True)
+            return redirect(url_for('views.home'))
+
+    return render_template("sign-up.html", user=current_user)
+
